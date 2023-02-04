20ae97ae4553d8dc47016e1f0d6e83db1d3e5f99
nadim <sayemnadim88@gmail.com>
2023-02-04 22:14:13 +0600

    lo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider ,signInWithPopup,signOut  } from "firebase/auth";
+ //import * as firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
 
+
+
+//firebaseConfig.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 function App() {
+  const [user,setUser]= useState({
+    isSingnedIn: false,
+    name:'',
+    email:'',
+    photo:''
+    
+
+  })
+  const auth = getAuth();
+  const provider = new GoogleAuthProvider();
+  const handleSignIn =() =>{
+    signInWithPopup(auth, provider)
+    .then((res) =>{
+      const {displayName,photoURL,email} = res.user;
+      const signedInUser = {
+        isSignedIn: true,
+        name: displayName,
+        email: email,
+        photo: photoURL
+
+      }
+      setUser(signedInUser);
+      console.log(displayName,photoURL,email);
+    })
+    .catch(err =>{
+      console.log(err);
+      console.log(err.message);
+
+    })
+  }
+
+  const handleSignOut = () => {
+    
+signOut(auth).then(() => {
+  const signedOutUser = {
+    isSignedIn : false,
+    name:'',  
+    photo:'',
+    email:''
+  }
+  setUser(signedOutUser);
+  
+  console.log ('log out');
+}).catch((error) => {
+  
+});
+  }
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+       user.isSingnedIn ? <button onClick={handleSignOut}>Sign out</button>:
+        <button onClick={handleSignIn}>Sign in</button>
+      }
+      
+      {
+        user.isSingnedIn && <div>
+          <p>Wlcome, {user.name}</p>
+          <p>Your email: {user.email}</p>
+          <img src={user.photo} alt="" srcset="" />
+        </div>
+      }
     </div>
   );
 }
---
+++ src/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCyS_jpqWbap5RBYL1sVBLgvFWLULHtfdA",
+    authDomain: "ema-john-simple-a7675.firebaseapp.com",
+    projectId: "ema-john-simple-a7675",
+    storageBucket: "ema-john-simple-a7675.appspot.com",
+    messagingSenderId: "994187112614",
+    appId: "1:994187112614:web:721158c210241a505c8467"
+  };
+  export default firebaseConfig;
